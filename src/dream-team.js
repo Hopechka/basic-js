9453fb5257d64cf8cac105a53b0c7a4435da239f
Nadezhda Budarina <hopechka@gmail.com>
2022-03-16 14:21:43 +0300

    refactor: solving Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,35 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+  if (arr.length == 0) {
+    return false;
+  }
+  let newArr = [];
+  for (let name of arr) {
+    if (typeof name == 'string') {
+      name = name.trim();
+      let letter = name[0].toUpperCase();
+      newArr.push(letter);
+    }
+  }
+  return newArr.sort().join('');
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
