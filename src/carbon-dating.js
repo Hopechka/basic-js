4d15ae56b6b130e99969cfee39b089273e1863d3
Nadezhda Budarina <hopechka@gmail.com>
2022-03-16 12:26:42 +0300

    refactor: npm install and solving
    1. npm install
    2. solving for the count-cats
    3. solving for the carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,36 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(str) {
+  if (typeof str != 'string') {
+    return false;
+  }
+  if (!parseInt(str)) {
+    return false;
+  }
+  let n = Number(str);
+  if (n < 0) {
+    return false;
+  }
+  let k = Math.log(2) / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / n) / k);
+  if (t < 0) {
+    return false;
+  }
+  return t;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
