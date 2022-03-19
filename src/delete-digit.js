57416cbdd8de0fc3b1249f45337b311765d0c4a9
Nadezhda Budarina <hopechka@gmail.com>
2022-03-19 14:18:19 +0300

    refactor: add solving for Delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  arr = [];
+  n = n.toString();
+  for (let i = 0; i < n.length; i++) {
+    let str = n.slice(0, i) + n.slice(i + 1);
+    arr.push(str);
+  }
+  return Math.max(...arr);
 }
 
+// console.log(deleteDigit(152)); // 52);
+// console.log(deleteDigit(1001)); // 101);
+// console.log(deleteDigit(10)); // 1);
+// console.log(deleteDigit(222219)); // 22229);
+// console.log(deleteDigit(109)); // 19);
+// console.log(deleteDigit(342)); // 42);
+
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
