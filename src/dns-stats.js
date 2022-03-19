f19d41b79d0da6c9cd918afa20a5847e78eacb96
Nadezhda Budarina <hopechka@gmail.com>
2022-03-19 17:15:52 +0300

    refactor: add solving for DNS stat

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  let arr = [];
+  domains.map((item) => {
+    arr.push(item.split('.').reverse());
+  });
+  //console.log(arr);
+
+  arr.forEach((item) => {
+    let name = '.' + item[0];
+
+    for (let i = 0; i <= arr.length; i++) {
+      if (!name.includes('undefined')) {
+        if (name in obj) {
+          ++obj[name];
+        } else {
+          obj[name] = 1;
+        }
+
+        name += '.' + item[i + 1];
+      }
+    }
+  });
+
+  return obj;
 }
 
+// console.log(getDNSStats(['code.yandex.ru', 'music.yandex.ru', 'yandex.ru'])); // {'.ru': 3,'.ru.yandex': 3,'.ru.yandex.code': 1,'.ru.yandex.music': 1,}
+// console.log(getDNSStats(['epam.com'])); // { '.com': 1, '.com.epam': 1 });
+// console.log(getDNSStats(['epam.com', 'info.epam.com'])); // { '.com': 2, '.com.epam': 2, '.com.epam.info': 1 });
+// console.log(getDNSStats([])); // {});
+
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
