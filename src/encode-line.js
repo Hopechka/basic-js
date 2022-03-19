428b61d30acafdafafc754ed08921de22f6450ed
Nadezhda Budarina <hopechka@gmail.com>
2022-03-19 18:36:49 +0300

    refactor: add solving for Encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    //console.log(str[i]);
+    //console.log(new RegExp(`${str[i]}{2,}`).test(str));
+    //проверка на повторяющиеся буквы
+    if (new RegExp(`${str[i]}{2,}`).test(str)) {
+      //вывожу массив с совпадениями и измеряю длинну совпадающих букв
+      let matches = str.match(new RegExp(`${str[i]}{2,}`));
+      //console.log(matches[0].length);
+      arr.push(matches[0].length);
+      arr.push(str[i]);
+      i += matches[0].length - 1;
+    } else {
+      arr.push(str[i]);
+    }
+  }
+  return arr.join('');
 }
 
+// console.log(encodeLine('aaaatttt')); //'4a4t');
+// console.log(encodeLine('aabbccc')); //'2a2b3c');
+// console.log(encodeLine('abbcca')); //'a2b2ca');
+// console.log(encodeLine('xyz')); //'xyz');
+// console.log(encodeLine('')); //'');
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
