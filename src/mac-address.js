32d133e3f3d73ba7551fb491e8295b017ec60a0a
Nadezhda Budarina <hopechka@gmail.com>
2022-03-21 10:04:32 +0300

    refactor:add solving for mac-address

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
+ * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F)) //
  * separated by hyphens.
  *
  * Your task is to check by given string inputString
@@ -14,10 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let str = '1234567890ABCDEF-';
+  let arr = n.split('-');
+  if (arr.length != 6) {
+    return false;
+  }
+  for (let i = 0; i < n.length; i++) {
+    return str.includes(n[i]);
+  }
 }
+
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
