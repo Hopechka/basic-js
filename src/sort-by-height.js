b29c2e1b6a6e6e1544ac5864da9fac6cf50e8d35
Nadezhda Budarina <hopechka@gmail.com>
2022-03-21 15:35:23 +0300

    refactor: add solving for Sort by height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let newArr = [...arr];
+  newArr.sort((a, b) => a - b);
+  let answer = [];
+  let some = newArr.lastIndexOf(-1) + 1;
+  for (let i = 0; i < newArr.length; i++) {
+    if (arr[i] == -1) {
+      answer.push(-1);
+    } else {
+      answer.push(newArr[some]);
+      some += 1;
+    }
+  }
+  return answer;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
